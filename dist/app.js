7bb67558e2bc72dfca3e64cab4900647867ebebe
Greg Duncan <gregdd@outlook.com>
2017-07-06 16:51:45 +1000

    change cell size

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Board from './components/board'
+import './components/styles/app.scss'
 import injectTapEventPlugin from 'react-tap-event-plugin'
 injectTapEventPlugin()
 
@@ -11,10 +12,12 @@ class App extends React.Component {
   render () {
     return (
       <div>
-        <h1>Game of Life</h1>
-        <img alt="freeCodeCamp logo" src="https://www.freecodecamp.com/design-style-guide/img/freeCodeCamp-alternative.png" />
-        <p>a freeCodeCamp challange</p>
-        <p>by Greg Duncan</p>
+        <div className='centerMe'>
+          <img alt="freeCodeCamp logo" src="https://www.freecodecamp.com/design-style-guide/img/freeCodeCamp-alternative.png" />
+          <h1>Game of Life</h1>
+          <p>a freeCodeCamp challange</p>
+          <p>by Greg Duncan</p>
+        </div>
         <Board />
       </div>
     )
